52eaa725fd345ec4a366f26f00b7a979df9558f0
gustavuwe <gustavuwe.123@gmail.com>
2023-08-24 09:15:54 -0300

    some changes...

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -1,23 +1,40 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    resultImc: {
+    contextImc: {
         flex: 1,
-        marginTop: 15,
-        paddingTop: 60,
+        marginTop: 20,
+        paddingTop: 15,
         borderRadius: 50,
         alignItems: "center",
         width: "100%",
     },
-    numberImc: {
+    resultImc: {
         fontSize: 48,
         color: "#FF0043",
         fontWeight: "bold",
     },
-    information: {
+    titleResultImc: {
         fontSize: 18,
         color: "#FF0043",
         fontWeight: "bold",
+    },
+    boxSharebutton: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+        marginHorizontal: 30,
+    },
+    shared: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingBottom: 5,
+        paddingTop: 5,
+    },
+    sharedText: {
+        color: "#ffffff",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
     }
 })
 
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { 
+     View,
+     Text, 
+     TextInput, 
+     TouchableOpacity,
+     Vibration,
+     Pressable,
+     Keyboard,
+    } from 'react-native';
 import ResultImc from './ResultImc/';
 import styles from "./style"
 
@@ -10,9 +18,18 @@ const [weight, setWeight] = useState(null)
 const [messageImc, setMessageImc] = useState("Preencha o peso e altura")
 const [imc, setImc] = useState(null)
 const [textButton, setTextButton ] = useState("Calcular")
+const [errorMessage, setErrorMessage] = useState(null)
 
 function imcCalculator() {
-    return setImc((weight / (height * height)).toFixed(2))
+    let heightFormat = height.replace(",",".")
+    return setImc((weight / (heightFormat * heightFormat)).toFixed(2))
+}
+
+function verificationImc() {
+    if (imc == null) {
+        Vibration.vibrate();
+        setErrorMessage("campo obrigatório*")
+    }
 }
 
 function validationImc() {
@@ -22,8 +39,10 @@ function validationImc() {
         setWeight(null)
         setMessageImc("O seu imc é igual a:")
         setTextButton("Calcular Novamente")
+        setErrorMessage(null)
         return
     }
+    verificationImc()
 
     setImc(null)
     setTextButton("Calcular")
@@ -31,10 +50,11 @@ function validationImc() {
 }
 
     return (
-        <View style={styles.formContext}>
+        <Pressable onPress={Keyboard.dismiss} style={styles.formContext}>
             <View style={styles.form}>
 
                 <Text style={styles.formLabel}>Altura</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
                 <TextInput
                 style={styles.input}
                 onChangeText={setHeight}
@@ -44,6 +64,7 @@ function validationImc() {
                 />
 
                 <Text style={styles.formLabel}>Peso</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
                 <TextInput
                 style={styles.input}
                 onChangeText={setWeight}
@@ -62,6 +83,6 @@ function validationImc() {
                 </TouchableOpacity>
             </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc}/>
-        </View>
+        </Pressable>
     );
 }
